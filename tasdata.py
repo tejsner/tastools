4822b2b700e3e8fd8cfc8b035dafb5fb750dd988
Tim Tejsner <ttejsner@gmail.com>
2018-05-27 23:25:31 +0200

    update tasdata

--- tasdata.py
+++ tasdata.py
@@ -8,7 +8,14 @@ class tasdata:
         self.axes = []
         self.signal = None
         self.monitor = None
+        
+        # if given axes, asign the first one as our x-axis
+        if axes:
+            self.xaxis = axes[0]
+        else:
+            self.xaxis = False
 
+        # if given filenumbers, load data right away
         if filenumbers is not None:
             filenames = []
             for f in filenumbers:
@@ -35,9 +42,11 @@ class tasdata:
         """ update intensity and error """
         self.I = self.signal/self.monitor
         self.err = np.sqrt(self.signal)/self.monitor
+        if self.xaxis:
+            self.x = getattr(self, self.xaxis)
 
-    def load(self, filenames, format='ILL'):
-        """ load data and assign signal, monitor and any added axes """
+    def load_ill(self, filenames):
+        """ loads data from ILL TAS """
         self.measurements.append('M2')
         self.measurements.append('TIME')
                 
@@ -57,17 +66,92 @@ class tasdata:
             self.M2 = np.concatenate((self.M2, d['M2']))
             self.TIME = np.concatenate((self.TIME, d['TIME']))
 
-            for ax in self.axes:                
+            # if we have not defined an x-axis, take the scan axis from the first file
+            if not self.axes:
+                self.axes.append(d.dtype.names[1])
+                setattr(self, self.axes[0], np.array([]))
+                self.xaxis = self.axes[0]
+            
+            for ax in self.axes:
                 setattr(self, ax, np.concatenate((getattr(self, ax), d[ax])))
+
+
+    def load(self, filenames, fileformat='ILL'):
+        """ load data and assign signal, monitor and any added axes """
+        if fileformat == 'ILL':
+            self.load_ill(filenames)
 
         self.update()
 
-    def plot(self, axis):
-        plt.errorbar(getattr(self, axis), self.I, self.err, fmt='o')
-        plt.show()
+    def bin(self, bins, axis=None, method='avg'):
+        """ Bins the data along a certain axis 
+            Either specify a binsize (float) or a vector (array or list) with the actual bins 
+            methods: 'avg' takes the average of x-values in each bin.
+                     'center' uses the bin center as x-value
+
+            TODO: bin center, save binned x-axis in a seperate variabe
+        """
+        # use the scan-axis if not specified
+        if axis is None:
+            axis = self.xaxis
+
+        if isinstance(bins, float):
+            xmin = min(getattr(self, axis))
+            xmax = max(getattr(self, axis))
+            bins = np.arange(xmin-bins/2, xmax+bins, bins)
+        
+        inds = np.digitize(getattr(self, axis), bins)
+
+        # make lists of temporary axes (x) and measurements (y) with each element being initialized as an array
+        x = []
+        y = []
+        for ax in self.axes:
+            x.append(np.array([]))
+
+        for meas in self.measurements:
+            y.append(np.array([]))
+        
+        # fill up the temporary axes and measurments
+        for i in np.unique(inds):
+            # for axes we take the mean value in each bin
+            for j, ax in enumerate(self.axes):
+                x[j] = np.append(x[j], getattr(self, ax)[inds == i].mean())
+
+            # for measurements we add up the values
+            for k, meas in enumerate(self.measurements):
+                y[k] = np.append(y[k], getattr(self, meas)[inds == i].sum())
+
+        # update axes and mreasurements
+        for i, ax in enumerate(self.axes):
+            setattr(self, ax, x[i])
+
+        for j, meas in enumerate(self.measurements):
+            setattr(self, meas, y[j])
+
+        self.update()
+
+    def plot(self):
+        plt.errorbar(self.x, self.I, self.err, fmt='o')
+
+    def __sub__(self, other):
+        """ subtraction given that the two objects have intersecting bins """
+        pass
+
+    def __add__(self, other):
+        """ addition given that the two objects have intersecting bins """
+        pass
 
 # example
+d_40K = tasdata([18543,18578], prefix='data/rawdata/0')
+d_80K = tasdata([18548], prefix='data/rawdata/0')
+d_10K = tasdata([18536], prefix='data/rawdata/0')
+d_40K.bin(0.007, 'QH')
+d_80K.bin(0.007, 'QH')
+d_10K.bin(0.007, 'QH')
 
-d = tasdata([18530,18531,18532], prefix='data/rawdata/0', axes=['QH', 'QK'])
-print(d)
-d.plot('QH')
+# d_10K.plot()
+# d_40K.plot()
+# d_80K.plot()
+plt.plot(d_10K.x, d_10K.I-d_40K.I+0.005, 'o')
+plt.plot(d_10K.x, len(d_10K.x)*[0.005], '-')
+plt.show()
